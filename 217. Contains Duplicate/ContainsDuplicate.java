de5a2f34aa9b073f25334877d1a577f085bafb70
Chong Wang <wcssdy@gmail.com>
2017-10-16 23:06:54 +1300

    solution added

--- 217. Contains Duplicate/ContainsDuplicate.java
+++ 217. Contains Duplicate/ContainsDuplicate.java
@@ -4,7 +4,9 @@
  * @author: Chong Wang
  * Date: 15/10/17, 22:22
  */
- 
+
+//Solution 1
+//Time complexity: O(N^2), memory: O(1) 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         for(int i=0; i<nums.length; i++){
@@ -14,6 +16,20 @@ class Solution {
                 }
             }
             
+        }
+        return false;
+    }
+}
+
+//Solution 2
+//Time complexity: O(N lg N), memory: O(1) - not counting the memory used by sort
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }          
         }
         return false;
     }
